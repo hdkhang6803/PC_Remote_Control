0bcd9ef0c187252ff7022416289f373dd0043275
MPhuongLe <mphuongle30@gmail.com>
2023-05-24 12:25:53 +0700

    remove .cpp, .h files

--- keyboard_track.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <codecvt>
-#include <Windows.h>
-#include <iostream>
-
-std::wstring VirtualKeyCodeToString(UCHAR virtualKey);
-LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
-void tracking_keyboard();
